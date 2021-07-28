457781854475fec32840d963e870fac6d5a1e93b
antony-jr <antony-jr@users.noreply.github.com>
2021-07-28 15:57:27 +0000

    deploy: a6df951612cab8541f830186ed1e367eede1bf76

--- _next/static/j1r99bs4a1qUh-xt53d0n/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/583-196bef5aa498f3e442d4.js","static/chunks/pages/index-1669e1d36db6fe325148.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
