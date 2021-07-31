c7340fc6d5fb96ec9bf97af1867c628cd3930683
antony-jr <antony-jr@users.noreply.github.com>
2021-07-31 04:42:58 +0000

    deploy: 83924f6d2c286fe8c19097a25dd1e35a4d63aaf7

---
+++ _next/static/-YbKWucF1WyVsp0t8a8aO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/319-77a9b50fffbb07abd45c.js","static/chunks/pages/index-d809ea38d3f9cec1ba72.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/B6QBPeQuZ4Idl7udY27O4/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/871-a520d940bb8e3f95f529.js","static/chunks/pages/index-c1362fb96d976111192d.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
