8f3525442eded1b7635ab89fb7fca49c46066655
boostwilddev <88891495+boostwilddev@users.noreply.github.com>
2021-08-13 19:02:51 +0530

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,10 +1,16 @@
-import { Box, Stack, Heading } from "@chakra-ui/react";
+import { Box, Stack, Image, Heading } from "@chakra-ui/react";
 
 export default function Logo(props) {
   return (
     <Box>
       <Stack align="center" direction="row" spacing={1}>
-        <Heading as="h1" fontSize={["lg", "2xl", "2xl", "2xl"]} color="#6d64e8">
+         <Image
+            align="center"
+            w={["35px", "40px", "40px", "50px", "50px"]}
+            objectFit="cover"
+            src="/logo.png"
+            alt="nav_logo"/>
+         <Heading as="h1" fontSize={["lg", "2xl", "2xl", "2xl"]} color="#6d64e8">
           BoostWild
         </Heading>
       </Stack>
