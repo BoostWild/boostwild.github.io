089a2c5066aadd71b26e828079205276f3e3e6df
PrinceJabez <PrinceJabez@users.noreply.github.com>
2021-08-14 03:58:25 +0000

    deploy: 2972ab303aebb4a010f973a717ba193eed5795a6

--- _next/static/lgyS0Kt_8bCvEK4BmKs98/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/319-77a9b50fffbb07abd45c.js","static/chunks/pages/index-1bd7acbf79d61f8a7d9d.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
